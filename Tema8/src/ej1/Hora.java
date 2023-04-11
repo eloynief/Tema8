ab3745bb9a551c719923122283e57fe9eb1bfd9f
enieves <enieves@106-02.info2>
2023-04-11 14:19:28 +0200

    Prácticas terminar

---
+++ Tema8/src/ej1/Hora.java
@@ -0,0 +1,65 @@
+package ej1;
+
+public class Hora {
+	
+	
+	//creación de variables
+	protected int hora=0;
+	protected int minuto=0;
+	
+	
+	public int getHora() {
+		return hora;
+	}
+
+	public int getMinuto() {
+		return minuto;
+	}
+
+	
+	
+	public void inc() {
+		if (minuto>59){
+			hora++;
+		}
+	}
+	
+	boolean setMinuto(int min) {
+		
+		
+		boolean esvalido=false;
+		if(min>=0||min<60) {
+		esvalido=true;	
+		}
+		return esvalido;
+		
+		
+	}
+	
+	//
+	boolean setHora(int h) {
+		
+		
+		boolean esvalido=false;
+		if(h>=0||h<60) {
+		esvalido=true;	
+		}
+		return esvalido;
+		
+		
+	}
+	
+	
+	
+	public String toString() {
+		
+		
+		
+		return 
+	}
+	
+	
+	
+	
+	
+}
---
+++ Tema8/src/ej1/HoraExacta.java
@@ -0,0 +1,35 @@
+package ej1;
+
+public class HoraExacta {
+	enum hora{amogy};
+	
+	/*sosososososososososososososososoososososososossosososososososososos
+	sososososososososososososososososoosos
+	sosososososoosososossososoososososo
+	sosososososososososososososososososo
+	sosososososoosososososoososososo
+	sososososososossoososososososososos
+	sososososososososossosososososo
+	sosoosososososososososososososos
+	sososososososososososososososo
+	ssososososososososososososososo
+	ssososososososososososososos
+	sosososoososososososososososos
+	ssososososososososososososo
+	sossosososososososososososos
+	sososososososososososososo*/
+	
+	HoraExacta(int hora,int minuto,int segundo){
+		
+	}
+	
+	public setSegundo(int valor){
+		if 
+		
+	}
+	
+	
+	
+	
+	
+}
