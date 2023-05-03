05386fbef1ce8accf0e24e8f7d6d01acbf672c9f
Usuario <Usuario@DESKTOP-9C2U3EN>
2023-05-03 22:58:38 +0200

    Test1

---
+++ Tema8/src/ejercicio4/Main.java
@@ -0,0 +1,12 @@
+package ejercicio4;
+
+public class Main {
+	//acuerdate
+	//enum color {blanco, negro, rojo, azul, gris};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Electodomestico arrayobjetos=new Electodomestico();
+	}
+
+}
